d41c6e2323e03e7853eb1e9ca528ea65116f714f
Evan Raker <er306632@students.davenport.k12.ia.us>
2019-10-11 09:08:58 -0500

    update newest

--- src/EvanWorkplace.java
+++ src/EvanWorkplace.java
@@ -1,7 +1,29 @@
+import java.util.Scanner;
+
 public class EvanWorkplace {
 
     public static void main(String[] args){
 
+        pickRace();
+    }
+
+    public static void pickRace(){
+
+        String playerRace;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Pick your characters race. >>>");
+        playerRace = input.toString();
+        System.out.println("your race is " + playerRace);
+    }
+
+    public static void pickClass(){
 
     }
+
+    public static void PickStats(){
+
+    }
+
 }
