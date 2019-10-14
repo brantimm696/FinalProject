7043425e7846de2e6c25ec1e988afe0ea2597432
Evan Raker <er306632@students.davenport.k12.ia.us>
2019-10-14 09:49:36 -0500

    almost

--- src/EvanWorkplace.java
+++ src/EvanWorkplace.java
@@ -15,9 +15,42 @@ public class EvanWorkplace {
 
         do
         {
-            System.out.println("Pick your characters race. >>>" +
-                    "");
+            System.out.println("Pick your characters race. >>>"
+            		+"\nDwarf +2 Constitution, +2, Wisdom, -2 Charisma."
+                    + "\nElf +2 Dexterity, +2 Intelligence, -2 Constitution"
+                    + "\nGnome +2 Constitution, +2 Charisma, -2 Strength"
+                    + "\nHalf-Elf +2 Any Stat"
+                    + "\nHalfling +2 Dexterity, +2 Charisma, -2 Strength"
+                    + "\nHalf-Orc +2 Any Stat"
+                    + "\nHuman +2 Any Stat");
             playerRace = input.next();
+            
+            if(playerRace.equalsIgnoreCase("dwarf")) {
+            	playerRace = "Dwarf";
+            }
+            else if(playerRace.equalsIgnoreCase("elf")) {
+            	playerRace = "Elf";
+            }
+            else if(playerRace.equalsIgnoreCase("Gnome")) {
+            	playerRace = "Gnome";
+            }
+            else if(playerRace.equalsIgnoreCase("half-elf")) {
+            	playerRace = "Half-Elf";
+            }
+            else if(playerRace.equalsIgnoreCase("halfling")) {
+            	playerRace = "Halfling";
+            }
+            else if(playerRace.equalsIgnoreCase("half-orc")) {
+            	playerRace = "Half-Orc";
+            }
+            else if(playerRace.equalsIgnoreCase("human")) {
+            	playerRace = "Human";
+            }
+            else
+            {
+            	playerRace = "empty";
+            	System.out.println("Your entry was invalid");
+            }
 
         }while(playerRace == "empty");
 
